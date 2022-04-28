28de7c3b4e0631f3424853775ca7f9c249f96d37
Thomas Tammoja <thomas.tammoja@carrentalgateway.com>
2022-04-29 02:18:49 +0300

    Added more values to insert

--- frontend/src/components/InputCity.js
+++ frontend/src/components/InputCity.js
@@ -1,15 +1,19 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 
 const InputCity = () => {
 
     const [city, setCity] = useState("");
+    const [longitude, setLongitude] = useState(0);
+    const [latitude, setLatitude] = useState(0);
+
+    const country_id = useParams().id;
 
     const onSubmitForm = async (e) => {
         e.preventDefault();
         try {
 
-            const country_id = 256;
-            const body = { city, country_id }
+            const body = { city, country_id, longitude, latitude }
             const response = await fetch("http://localhost:8000/api/add/city", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
@@ -27,19 +31,38 @@ const InputCity = () => {
         <div>
             <h1 className="text-center mt-5">Linnad</h1>
             <h4 className="text-left mt-5">Lisa linn</h4>
-            <form className="d-flex mt-2" onSubmit={onSubmitForm}>
+
+            <h6 className="text-left mt-2">Linn</h6>
+            <form className="mt-2">
                 <input
                     type="text"
                     className="form-control"
                     value={city}
                     onChange={e => setCity(e.target.value)}
                 />
+            </form>
+
+            <h6 className="text-left mt-2">Laiuskraad</h6>
+            <form className="mt-2">
+                <input
+                    type="text"
+                    className="form-control"
+                    value={latitude}
+                    onChange={e => setLatitude(e.target.value)}
+                />
+            </form>
+
+            <h6 className="text-left mt-2">Pikkuskraad</h6>
+            <form className="d-flex mt-2" onSubmit={onSubmitForm}>
+                <input
+                    type="text"
+                    className="form-control"
+                    value={longitude}
+                    onChange={e => setLongitude(e.target.value)}
+                />
                 <button className="btn btn-success">Lisa</button>
             </form>
         </div>
-
-
-
     )
 }
 
